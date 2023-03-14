5ac91c9d8418e7c74fee0de841c4a6342f9f92fc
lewisthagichu <lewisthagichu254@gmail.com>
2023-03-14 07:53:03 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,12 +25,15 @@ char *str_concat(char *s1, char *s2)
 
 	result = (char *)malloc(sizeof(char) * len + 1);
 
+	if (result == NULL)
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 	for (j = 0; j < len2; j++)
-		result[i + j] = s2[j];
+		result[i] = s2[j];
 
-	result[i + j] = '\0';
+	result[len] = '\0';
 
 	return (result);
 }
