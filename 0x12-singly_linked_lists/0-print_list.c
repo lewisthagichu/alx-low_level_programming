52af8048069807f16c8b87bab46e8b84e890f646
lewisthagichu <lewisthagichu254@gmail.com>
2023-03-30 11:11:52 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 
 /**
  * print_list - check the code for Holberton School students.
- * @h: name of the list
+ * @h: name of the list.
  * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
