51968d6774694d8be4e1eb75a450fcf67e522ede
lewisthagichu <lewisthagichu254@gmail.com>
2023-04-10 13:43:04 -0700

    assignment 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	a = open(filename, O_RDONLY);
 	b = read(a, buffers, letters);
-	c = write(STDOUT_FILENO, buffer, b);
+	c = write(STDOUT_FILENO, buffers, b);
 
 	if (a == -1 || b == -1 || c == -1 || c != b)
 	{
